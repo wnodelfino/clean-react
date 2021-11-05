8130fd71bbbfe158f2a2a90f5ac322a9c96cdd87
Wellynton Delfino <wnodelfino@gmail.com>
2021-11-05 19:25:03 -0300

    chore: ajust test

--- src/pressentation/pages/login/login.spec.tsx
+++ src/pressentation/pages/login/login.spec.tsx
@@ -3,14 +3,9 @@ import { render } from "@testing-library/react";
 import Login from "./login";
 
 describe("Login Component", () => {
-<<<<<<< HEAD
   test("Should not render spinner and error on start", () => {
     const { getByTestId } = render(<Login />);
     const errorWrap = getByTestId("error-wrap");
     expect(errorWrap.childElementCount).toBe(0);
-=======
-  test("", () => {
-    render(<Login />);
->>>>>>> 48c6199cee71f09cd423aad58789c4b021a46059
   });
 });
