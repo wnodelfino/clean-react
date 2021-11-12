853d919a8d0069d39a335c57bcd5fd1b086073cb
Wellynton Delfino <wnodelfino@gmail.com>
2021-11-11 22:53:43 -0300

    feat: ensure Login calls Authentication with correct values

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -8,12 +8,14 @@ import {
 } from "@/presentation/components";
 import Context from "@/presentation/contexts/form/form-context";
 import { Validation } from "@/presentation/protocols/validation";
+import { Authentication } from "@/domain/usecases";
 
 type Props = {
   validation?: Validation;
+  authentication?: Authentication;
 };
 
-const Login: React.FC<Props> = ({ validation }: Props) => {
+const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     email: "",
@@ -34,6 +36,7 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
     setState({ ...state, isLoading: true });
+    authentication.auth({ email: state.email, password: state.password });
   };
 
   return (
