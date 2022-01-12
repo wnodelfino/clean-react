bc56bb734bc6500a6bb53305e06d1a6927622489
Wellynton Delfino <wnodelfino@gmail.com>
2022-01-12 10:52:27 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
 import { InvalidFieldError } from "@/validation/errors";
 import { EmailValidation } from "./email-validation";
+import faker from "faker";
 
 describe("EmailValidation", () => {
   test("Should Return error if email is invalid", () => {
-    const sut = new EmailValidation("email");
-    const error = sut.validate("");
+    const sut = new EmailValidation(faker.random.word());
+    const error = sut.validate(faker.random.word());
     expect(error).toEqual(new InvalidFieldError());
+  });
+
+  test("Should Return falsy if email is vailid", () => {
+    const sut = new EmailValidation(faker.random.word());
+    const error = sut.validate(faker.internet.email());
+    expect(error).toBeFalsy();
   });
 });
