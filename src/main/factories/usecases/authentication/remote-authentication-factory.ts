def0108220112693a5baac23bb6df57ca9a25b8b
Wellynton Delfino <wnodelfino@gmail.com>
2022-01-14 11:37:34 -0300

    refactor: move factories to their own files

---
+++ src/main/factories/usecases/authentication/remote-authentication-factory.ts
@@ -0,0 +1,7 @@
+import { makeAxiosHttpClient, makeApiUrl } from "@/main/factories/http";
+import { RemoteAuthentication } from "@/data/usecases/authetication/remote-authentication";
+import { Authentication } from "@/domain/usecases";
+
+export const makeRemoteAuthentication = (): Authentication => {
+  return new RemoteAuthentication(makeApiUrl(), makeAxiosHttpClient());
+};
