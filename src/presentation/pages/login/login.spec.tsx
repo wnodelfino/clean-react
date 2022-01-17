da187c75e0a6d5a1013abe530946d2251e5266e1
Wellynton Delfino <wnodelfino@gmail.com>
2022-01-17 09:40:01 -0300

    chore: add files to coverage ignore

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -10,7 +10,7 @@ import {
   cleanup,
   waitFor,
 } from "@testing-library/react";
-import Login from "./login";
+import { Login } from "@/presentation/pages";
 import { ValidationStub, AuthenticationSpy } from "@/presentation/test";
 import { InvalidCredentialsError } from "@/domain/errors";
 
