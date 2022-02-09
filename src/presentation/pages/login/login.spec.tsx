4bb7de2078f64fb588a6ce1e60570861d22b2fe8
Wellynton Delfino <wnodelfino@gmail.com>
2022-02-09 11:12:40 -0300

    test: ensure Login present error if SaveaccessToken throws

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -209,6 +209,17 @@ describe("Login Component", () => {
     expect(history.location.pathname).toBe("/");
   });
 
+  test("Should present error if SaveAccessToken fails", async () => {
+    const { sut, saveAccessTokenMock } = makeSut();
+    const error = new InvalidCredentialsError();
+    jest
+      .spyOn(saveAccessTokenMock, "save")
+      .mockReturnValueOnce(Promise.reject(error));
+    await simulateValidSubmit(sut);
+    testElementText(sut, "main-error", error.message);
+    testErrorWrapCount(sut, 1);
+  });
+
   test("Should go to signup page", async () => {
     const { sut } = makeSut();
     const register = sut.getByTestId("signup");
