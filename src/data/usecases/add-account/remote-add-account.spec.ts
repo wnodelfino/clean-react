91e4d71e234b87f203086afc7c471fbeb7c50c7d
Wellynton Delfino <wnodelfino@gmail.com>
2022-02-11 10:00:58 -0300

    test: ensure RemoteAddAccount return an AccountModel on success

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -2,7 +2,7 @@ import { RemoteAddAccount } from "./remote-add-account";
 import { HttpPostClientSpy } from "@/data/test";
 import { AddAccountParams } from "@/domain/usecases";
 import { AccountModel } from "@/domain/models";
-import { mockAddAccountParams } from "@/domain/test";
+import { mockAccountModel, mockAddAccountParams } from "@/domain/test";
 import { HttpStatusCode } from "@/data/protocols/http";
 import { EmailInUseError, UnexpectedError } from "@/domain/errors";
 import faker from "faker";
@@ -73,5 +73,16 @@ describe("RemoteAddAccount", () => {
     };
     const promise = sut.add(mockAddAccountParams());
     await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test("Should return an AccountModel if HttpPostClient returns 200", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    const httpResult = mockAccountModel();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult,
+    };
+    const account = await sut.add(mockAddAccountParams());
+    await expect(account).toEqual(httpResult);
   });
 });
