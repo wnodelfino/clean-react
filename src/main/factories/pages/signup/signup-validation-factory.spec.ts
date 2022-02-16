8bbe1b585a1ff53113c4e07856f96a654705cfc5
Wellynton Delfino <wnodelfino@gmail.com>
2022-02-16 10:53:18 -0300

    test: add SignUpValidation factory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,17 @@
+import { ValidationComposite } from "@/validation/validators";
+import { ValidationBuilder as Builder } from "@/validation/validators/builder/validation-builder";
+import { makeSignUpValidation } from "./signup-validation-factory";
+
+describe("SignUpValidationFactory", () => {
+  test("Should make ValidationCompósite with correct validatons", () => {
+    const composite = makeSignUpValidation();
+    expect(composite).toEqual(
+      ValidationComposite.build([
+        ...Builder.field("email").required().min(4).build(),
+        ...Builder.field("email").required().email().build(),
+        ...Builder.field("password").required().min(5).build(),
+        ...Builder.field("passwordConfirmation").required().min(5).build(),
+      ])
+    );
+  });
+});
